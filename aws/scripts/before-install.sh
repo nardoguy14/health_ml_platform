1e7a03edb1bdd490722d1abc4c123689d54516de
nardoguy14 <nardoarevalo@me.com>
2024-01-25 23:09:10 -0800

    moving app spec file

--- aws/scripts/before-install.sh
+++ aws/scripts/before-install.sh
@@ -2,13 +2,13 @@
 set -xe
 
 # Delete the old  directory as needed.
-if [ -d /usr/local/codedeployresources ]; then
+if [ -d /usr/local/app/ ]; then
     rm -rf /usr/local/app/
 fi
 
 mkdir -vp /usr/local/app
 
-aws s3 cp s3://nardomlstack-webappdeploy?mentbucket-00batakrvsbh/app.zip /usr/local/app
+#aws s3 cp s3://nardomlstack-webappdeploymentbucket-00batakrvsbh/app.zip /usr/local/app
 
 ls
 
