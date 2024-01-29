803b056713b730caa3e3b619293197b0a787f1b8
nardoguy14 <nardoarevalo@me.com>
2024-01-29 02:03:46 -0800

    feature/3 deleting eb parts

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -7,4 +7,4 @@ cd /mnt
 . .venv/bin/activate
 sudo chmod 777 /mnt
 
-gunicorn -b 0.0.0.0:8080 -p uvicorn.pid -k uvicorn.workers.UvicornWorker app.main:app
+gunicorn -b 0.0.0.0:8080 -p uvicorn.pid -k uvicorn.workers.UvicornWorker app.main:app --daemon
