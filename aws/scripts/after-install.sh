7ba19ed1a416a67f5a2b7f697b2dbd351d4c8f2a
nardoguy14 <nardoarevalo@me.com>
2024-01-30 04:56:13 -0800

    updating cloudformation template

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -8,7 +8,6 @@ aws s3 cp s3://nardosfinalml-webappdeploymentbucket-5dtphqts3nhh/app.zip .
 
 unzip app.zip
 rm app.zip
-mv app/ml.service /etc/systemd/system/ml.service
 sudo chmod 777 /etc/systemd/system/ml.service
 
 
