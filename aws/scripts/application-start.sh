b658fa52d55dc0f74292b5341defe0a396ac36db
nardoguy14 <nardoarevalo@me.com>
2024-01-31 02:16:25 -0800

    adding in new bucket

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -7,4 +7,6 @@ cd /mnt
 . .venv/bin/activate
 sudo chmod 777 /mnt
 
+. /etc/environment
+
 gunicorn -b 0.0.0.0:8080 -p uvicorn.pid -k uvicorn.workers.UvicornWorker app.main:app --daemon --error-logfile logs.txt --capture-output
--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -3,5 +3,5 @@ set -x
 
 if [ -e "/mnt/uvicorn.pid" ]; then
     pid=$(cat /mnt/uvicorn.pid)
-    kill -9 $pid
+    sudo kill -9 $pid
 fi
