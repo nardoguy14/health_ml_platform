d1e8fe8258e50d419a5002998db90b443e43c4e1
nardoguy14 <nardoarevalo@me.com>
2024-01-31 03:07:55 -0800

    adding in new bucket

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -8,5 +8,6 @@ cd /mnt
 sudo chmod 777 /mnt
 
 . /etc/environment
+export STORE_IN_S3=1
 
 gunicorn -b 0.0.0.0:8080 -p uvicorn.pid -k uvicorn.workers.UvicornWorker app.main:app --daemon --error-logfile logs.txt --capture-output
