79d6b659fc36ebb345a7f1a419f7810ed88fe8cb
nardoguy14 <nardoarevalo@me.com>
2024-02-02 05:14:44 -0800

    feature/7 adding inference api endpoint

--- app/controllers/job_controller.py
+++ app/controllers/job_controller.py
@@ -1,7 +1,10 @@
+import tempfile
 from fastapi import APIRouter
 
 from app.domain.jobs import TrainingJob
 from app.services.jobs_service import JobsService
+from fastapi import UploadFile, APIRouter, File
+import shutil
 
 job_router = APIRouter()
 
@@ -12,3 +15,13 @@ jobs_service = JobsService()
 async def create_job(training_model: TrainingJob):
     training_job = await jobs_service.create_job(training_model)
     return training_job
+
+@job_router.post("/inference/model/{model_name}")
+async def run_inference(model_name: str, file: UploadFile = File(...)):
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        shutil.copyfileobj(file.file, temp_file)
+        temp_file.seek(0)
+        result = await jobs_service.run_inference_job(temp_file, model_name)
+        return {
+            "result": result
+        }
--- app/repositories/jobs_repository.py
+++ app/repositories/jobs_repository.py
@@ -15,6 +15,22 @@ class JobsRepository():
             session.commit()
             return training_model_db
 
-    async def get_training_job(self, name: str):
+    async def get_training_job_by_model_id(self, mode_id: int) -> TrainingJobsDB:
         with create_db_session() as session:
+<<<<<<< Updated upstream
             return session.query(TrainingJobsDB).filter(TrainingJobsDB.name == name).all()
+=======
+            return session.query(TrainingJobsDB).filter(TrainingJobsDB.model_id == mode_id).all()[0]
+
+    async def get_training_job_by_job_id(self, job_id: str) -> TrainingJobsDB:
+        with create_db_session() as session:
+            return session.query(TrainingJobsDB).filter(TrainingJobsDB.job_id == job_id).all()[0]
+
+    async def update_job_status(self, job_id: str, status: str):
+        with create_db_session() as session:
+            training_job: TrainingJobsDB = self.get_training_job_by_job_id(job_id)
+            training_job.status = status
+            session.merge(training_job)
+            session.commit()
+            return training_job
+>>>>>>> Stashed changes
